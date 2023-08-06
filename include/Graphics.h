5cffc42184b8631340f60bc6a1044f32fbfb3cac
uoay <67417191+uoay@users.noreply.github.com>
2023-08-06 20:02:26 +0800

    链接库

--- include/Graphics.h
+++ include/Graphics.h
@@ -6,10 +6,6 @@
 
 #include "GameException.h"
 
-#pragma comment(lib,"d3dcompiler.lib")
-#pragma comment(lib, "D3D12.lib")
-#pragma comment(lib, "dxgi.lib")
-
 #define GFX_EXPECTION(errorCode) Graphics::Exception(__FILE__, __LINE__, errorCode);
 
 class Graphics {
